61f9b228dcc578f46527ed570233dffb96de9425
Johannes Huemer <johannes.huemer@ait.ac.at>
2025-09-30 15:23:42 +0200

    Update logging level
    Signed-off-by: Johannes Huemer <johannes.huemer@ait.ac.at>

--- driver/src/sick_scansegment_xd/msgpack_exporter.cpp
+++ driver/src/sick_scansegment_xd/msgpack_exporter.cpp
@@ -214,7 +214,7 @@ bool sick_scansegment_xd::MsgPackExporter::RunCb(void)
                         ((msg_exported_counter%100) == 0);
                     if (msg_udp_received_counter != msg_exported_counter && msg_cnt_delta > msg_cnt_delta_max && do_print_message) // Test mode only, multiScan emulator must be started after lidar3d_multiscan_recv
                     {
-                        ROS_INFO_STREAM("MsgPack/Compact-Exporter::Run(): " << msg_udp_received_counter << " udp messages received, " << msg_exported_counter << " messages exported, " << (100.0 * packages_lost_rate) << "% package lost");
+                        ROS_DEBUG_STREAM("MsgPack/Compact-Exporter::Run(): " << msg_udp_received_counter << " udp messages received, " << msg_exported_counter << " messages exported, " << (100.0 * packages_lost_rate) << "% package lost");
                         msg_cnt_delta_max = msg_cnt_delta;
                         last_print_timestamp = fifo_clock::now();
                     }
@@ -227,15 +227,15 @@ bool sick_scansegment_xd::MsgPackExporter::RunCb(void)
                     double msg_exported_rate = (double)msg_exported_counter / sick_scansegment_xd::Fifo<ScanSegmentParserOutput>::Seconds(recv_start_timestamp, fifo_clock::now());
                     if (m_verbose && ((msg_exported_counter%100) == 0 || sick_scansegment_xd::Fifo<ScanSegmentParserOutput>::Seconds(last_print_timestamp, fifo_clock::now()) > 0.1)) // avoid printing with more than 100 Hz
                     {
-                        ROS_INFO_STREAM("MsgPack/Compact-Exporter:   " << current_udp_fifo_size << " udp packages still in input fifo, " << current_output_fifo_size << " messages still in output fifo, current segment index: " << msgpack_output.segmentIndex);
-                        ROS_INFO_STREAM("MsgPack/Compact-Exporter: " << msg_udp_received_counter << " udp scandata messages received, " << msg_exported_counter << " messages exported (scan+imu), " << (100.0 * packages_lost_rate) << "% package lost.");
-                        ROS_INFO_STREAM("MsgPack/Compact-Exporter: max. " << max_count_udp_messages_in_fifo << " udp messages buffered, max " << max_count_output_messages_in_fifo << " export messages buffered.");
+                        ROS_DEBUG_STREAM("MsgPack/Compact-Exporter:   " << current_udp_fifo_size << " udp packages still in input fifo, " << current_output_fifo_size << " messages still in output fifo, current segment index: " << msgpack_output.segmentIndex);
+                        ROS_DEBUG_STREAM("MsgPack/Compact-Exporter: " << msg_udp_received_counter << " udp scandata messages received, " << msg_exported_counter << " messages exported (scan+imu), " << (100.0 * packages_lost_rate) << "% package lost.");
+                        ROS_DEBUG_STREAM("MsgPack/Compact-Exporter: max. " << max_count_udp_messages_in_fifo << " udp messages buffered, max " << max_count_output_messages_in_fifo << " export messages buffered.");
                         std::stringstream s;
                         s << "MsgPack/Compact-Exporter: " << msg_exported_counter << " messages exported at " << std::fixed << std::setprecision(3) << msg_exported_rate << " Hz, mean time: " 
                             << std::fixed << std::setprecision(3) << duration_datahandling_milliseconds.MeanMilliseconds() << " milliseconds/messages, " 
                             << "stddev time: " << duration_datahandling_milliseconds.StddevMilliseconds() << ", " << "max time: " << duration_datahandling_milliseconds.MaxMilliseconds() << " milliseconds between udp receive and messages export, "
                             << "histogram=[" << duration_datahandling_milliseconds.PrintHistMilliseconds() << "]";
-                        ROS_INFO_STREAM(s.str());
+                        ROS_DEBUG_STREAM(s.str());
                         last_print_timestamp = fifo_clock::now();
                     }
                 }
